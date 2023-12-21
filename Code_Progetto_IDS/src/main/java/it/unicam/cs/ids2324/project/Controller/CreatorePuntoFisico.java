a8150b689314a648d552cd4b225f91d673ec3273
MichelePolenta <michele.polenta@studenti.unicam.it>
2023-12-21 23:56:44 +0100

    Inserimento db nel progetto

--- Code_Progetto_IDS/src/main/java/it/unicam/cs/ids2324/project/Controller/CreatorePuntoFisico.java
+++ Code_Progetto_IDS/src/main/java/it/unicam/cs/ids2324/project/Controller/CreatorePuntoFisico.java
@@ -6,7 +6,6 @@ import it.unicam.cs.ids2324.project.Model.PuntoFisico;
 
 public class CreatorePuntoFisico {
 
-
     public POI creaPOI(Comune comune, String titolo, String descrizione, double lat, double lon) {
         return new PuntoFisico(comune,titolo,descrizione,lat,lon);
     }
