3842ac4006f875e8f13eacb9782dfbf912a7b717
MichelePolenta <michele.polenta@studenti.unicam.it>
2024-02-17 03:47:59 +0100

    Conversione in springboot e revisione

--- Code_Progetto_IDS/Database/public/public/ids_db_backup.sql
+++
@@ -1,8 +0,0 @@
-create table ids_db_backup
-(
-    c1 text
-);
-
-alter table ids_db_backup
-    owner to postgres;
-
--- Code_Progetto_IDS/src/main/java/it/unicam/cs/ids2324/project/Model/Ruolo.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.ids2324.project.Model;
-public enum Ruolo {
-    TURAUT,
-    CONTR,
-    CONTRAUT,
-    CUR,
-    ANIM,
-    GEST,
-}
